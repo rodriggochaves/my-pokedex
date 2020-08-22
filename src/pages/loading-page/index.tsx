612d5b852e36776f60c2d20fe22063a2a695a9ce
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2020-08-22 11:12:17 -0300

    create Loading Page

---
+++ src/pages/loading-page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.css";
+
+export default function LoadingPage() {
+  return (
+    <div className="loading-page">
+      <div className="spinner-border"></div>
+    </div>
+  );
+}
